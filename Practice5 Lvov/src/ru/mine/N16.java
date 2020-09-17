8b83c18931c22251d39bf83f626bba0599ff3103
Lvov Tinley <tin-97@yandex.ru>
2020-09-17 12:49:38 +0300

    fixed practice5

--- Practice5 Lvov/src/ru/mine/N16.java
+++ Practice5 Lvov/src/ru/mine/N16.java
@@ -4,18 +4,27 @@ import java.util.Scanner;
 
 public class N16 {
 
-    public static int maxQuan(int max, int quan, Scanner scanner) {
+    public static int maxQuan(int max, int quan) {
+        Scanner scanner = new Scanner(System.in);
         int k = scanner.nextInt();
         if (k == 0) {
             return quan;
         } else {
             if (max < k) {
-                max = k;
-                quan++;
+               quan =  maxQuan(k, 1);
 
             }
-            quan = maxQuan(max,quan, scanner);
-
+            else if (k == max) {
+                quan = maxQuan(max, ++quan);
+            }
+            else quan = maxQuan(max,quan);
+            /*if (k == Math.max(k, maxQuan(quan))) {
+                quan++;
+            }
+            else {
+                quan = 0;
+            }
+            return quan;*/
         }
         return quan;
 
@@ -26,7 +35,7 @@ public class N16 {
         int max = 0;
         int quan = 0;
         Scanner scanner = new Scanner(System.in);
-        int res = maxQuan(max,quan,scanner);
+        int res = maxQuan(max, quan);
         System.out.println("Max quantity: " + res);
 
     }
--- Practice5 Lvov/src/ru/mine/N17.java
+++ Practice5 Lvov/src/ru/mine/N17.java
@@ -4,25 +4,26 @@ import java.util.Scanner;
 
 public class N17 {
 
-    public static int maxRec(int max, Scanner scanner) {
+    public static int maxRec() {
+        Scanner scanner = new Scanner(System.in);
         int k = scanner.nextInt();
         if (k == 0) {
-            return max;
+            return 0;
         } else {
-            if (max < k) {
+            /*if (max < k) {
                 max = k;
 
             }
-            max = maxRec(max, scanner);
+            max = maxRec(max, scanner);*/
+            return Math.max(k,maxRec());
 
         }
-        return max;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
     int max = 0;
-        int res = maxRec(max,scanner);
+        int res = maxRec();
         System.out.println("The maximum number: " + res);
     }
 }
